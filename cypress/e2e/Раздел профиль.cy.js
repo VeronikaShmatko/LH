0bc712386245a4ddb66903efd632406a3f7cdd54
Вероника Шванская <veronikasvanskaa@MacBook-Air-Veronika.local>
2023-06-25 23:11:32 +0300

    newnwe

---
+++ cypress/e2e/Раздел профиль.cy.js
@@ -0,0 +1,18 @@
+import validUser from "/cypress/fixtures/validUser.json";
+import { loginMe } from "/cypress/support/utils";
+import { index } from "/cypress/support/index";
+
+describe("Страница авторизации пользователя", () => {
+  beforeEach(() => {
+    cy.visit("/auth");
+  });
+  it.only("Test 1:Проверка, что если в разделе 'Редактирование профиля' оставить все поля пустыми и нажать сохранить то высветятся валидационные ошибки", () => {
+    loginMe();
+    cy.contains("Профиль").click();
+    cy.contains("Редактировать профиль").click();
+    cy.get("#edit-profile-submit").click();
+    cy.contains("Минимальная длина 2 символа").should("be.visible");
+    cy.contains("E-mail не может быть пустым").should("be.visible");
+    cy.contains("Поле не может быть пустым").should("be.visible");
+  });
+});
